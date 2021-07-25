020ece018bd4ae3a581c33941c5aafa82607c33c
Vinay N <vinay@Vinays-MacBook-Pro.local>
2021-07-25 21:40:07 +0530

    Added main component

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Task from "./src/components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Today's Tasks */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+        <View style={styles.items}>
+          {/*This is where the tasks will go!*/}
+          <Task text="Task1" />
+          <Task text="Task2" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +22,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
+  },
+
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  items: {
+    marginTop: 30,
   },
 });
